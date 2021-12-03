509bec7044f333637c8fe69ca18f8ddc9f17f8e3
钟聪 <2849699894@qq.com>
2021-12-03 11:15:39 +0800

    Default Changelist

--- canlender/canlender/canlender.py
+++ canlender/canlender/canlender.py
@@ -1,38 +1,38 @@
 import datetime
 
-def shifourunnian(year):
+def is_leap_year(year):
     # 判断是否为闰年
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         return True
     else:
         return False
 
-def panduanyuefen(year, month):
+def get_num_of_days(year, month):
     if month in (1, 3, 5, 7, 8, 10, 12):
         return 31
     elif month in (4, 6, 9, 11):
         return 30
-    elif shifourunnian(year):
+    elif is_leap_year(year):
         return 29  # 闰年2月
     else:
         return 28  # 平年2月
 
-def youjitian(year,month):
+def get_total_days(year,month):
     days = 0
     for y in range(1800, year):
-        if shifourunnian(y):
+        if is_leap_year(y):
             days += 366
         else:
             days += 365
 
     for m in range(1, month):
-        days += panduanyuefen(year, m)
+        days += get_num_of_days(year, m)
 
     return days
 
 def get_start_day(year, month):
     # 返回当月1日是星期几，由1800.01.01是星期三推算
-    return 3 + youjitian(year, month) % 7
+    return 3 + get_total_days(year, month) % 7
 
 month_dict = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
               7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
@@ -50,11 +50,7 @@ def print_month_title(year, month):
     print('  Mon  Tue  Wed  Thu  Fri  Sat  Sun  ')
 
 def print_month_body(year, month):
-    '''
-    打印日历正文
-    格式说明：空两个空格，每天的长度为5
-    需要注意的是print加逗号会多一个空格
-    '''
+    # 打印日历正文
     i = get_start_day(year, month)
     # print(i)
     if i % 7 != 1:
@@ -64,7 +60,7 @@ def print_month_body(year, month):
     else:
         print('', end='')  # 打印行首的两个空格
 
-    for j in range(1, panduanyuefen(year, month) + 1):
+    for j in range(1, get_num_of_days(year, month) + 1):
         print('%5d' % j, end='')  # 宽度控制，4+1=5
 
 
